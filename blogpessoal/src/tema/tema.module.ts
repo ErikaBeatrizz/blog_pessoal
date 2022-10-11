0967966551d627ce6923398bc2b93b463bad80bc
ErikaBeatrizz <erikabeatriz_bea@hotmail.com>
2022-10-11 16:03:43 -0300

    Exemplo Nest JS

---
+++ blogpessoal/src/tema/tema.module.ts
@@ -0,0 +1,14 @@
+import { Controller, Module } from "@nestjs/common/decorators";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TemaController } from "./controllers/tema.controller";
+import { Tema } from "./entities/tema.entity";
+import { TemaService } from "./services/tema.service";
+
+@Module ({
+    imports: [TypeOrmModule.forFeature([Tema])],
+    providers: [TemaService],
+    controllers: [TemaController],
+    exports: [TypeOrmModule]
+})
+
+export class TemaModule {}
