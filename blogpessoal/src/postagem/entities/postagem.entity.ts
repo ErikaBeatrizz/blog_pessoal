532847bf1e49a8b439bd070fb47becf8439b98d5
Érika Beatriz <112709766+ErikaBeatrizz@users.noreply.github.com>
2022-10-13 15:03:57 -0300

    Update postagem.entity.ts

--- blogpessoal/src/postagem/entities/postagem.entity.ts
+++ blogpessoal/src/postagem/entities/postagem.entity.ts
@@ -2,16 +2,16 @@ import { IsNotEmpty } from "class-validator";
 import { Tema } from "src/tema/entities/tema.entity";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity({name: 'tb_postagens'})
+@Entity({name: 'tb_postagens'})     // decorator que chama a tabela principal
 
-export class Postagem {
-    @PrimaryGeneratedColumn()
+export class Postagem {                // exportando o estilo / classe
+    @PrimaryGeneratedColumn()           // primeira coluna
     id: number;
 
     
-    @IsNotEmpty()
-    @Column({length: 100, nullable: false})
-    titulo: string
+    @IsNotEmpty()                                   // nao pode ser nulo, tem que ter um dado 
+    @Column({length: 100, nullable: false})         // qntd caractere / nullable - não pode estar vazio
+    titulo: string                                  // tipo de entrada
 
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
